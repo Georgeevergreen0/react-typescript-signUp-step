fec9f5762048e79d5c7ef9075b8a6235893e6630
George Evergreen <georgeevergreen0@gmail.com>
2020-01-29 00:32:04 +0100

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Header } from 'semantic-ui-react';
+import Form from "./component/Form/Form"
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header as="h1" className="Header" >User Onboarding</Header>
+      <Form />
+    </Router>
   );
 }
 
